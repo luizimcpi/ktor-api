5dc3f59be43626325554689396db5095f73cfdd7
Luiz Evangelista <luiz.evangelista@b2wdigital.com>
2019-03-08 16:35:31 -0300

    feat: adicionando expiration time no token

--- src/model/SimpleJWT.kt
+++ src/model/SimpleJWT.kt
@@ -2,9 +2,12 @@ package com.devlhse.model
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import java.util.*
 
 open class SimpleJWT(val secret: String) {
+    private val validityInMs = 36_000_00 * 1 // 1 hour
     private val algorithm = Algorithm.HMAC256(secret)
     val verifier = JWT.require(algorithm).build()
-    fun sign(name: String): String = JWT.create().withClaim("name", name).sign(algorithm)
+    fun sign(name: String): String = JWT.create().withClaim("name", name).withExpiresAt(getExpiration()).sign(algorithm)
+    private fun getExpiration() = Date(System.currentTimeMillis() + validityInMs)
 }
